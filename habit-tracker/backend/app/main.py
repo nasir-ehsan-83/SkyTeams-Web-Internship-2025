707ce4c16b5d6973daac80e59307d13adb0df92d
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-19 14:44:58 +0430

    feat: add user API router to main.py file and add lifespan() to connect to database

--- habit-tracker/backend/app/main.py
+++ habit-tracker/backend/app/main.py
@@ -1,3 +1,14 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from backend.app.db.database import init_db
+from backend.app.routers import user
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(user.router)
