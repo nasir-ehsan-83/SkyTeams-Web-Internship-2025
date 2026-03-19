78b17b027cfc86dd0c0dfe1cd2eab84c9a2d96cf
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-19 14:06:07 +0430

    feat: implement production-ready User model
    - Add unique indexed email using Annotated and Beanie.
    - Ensure unique timestamps for each user using default_factory.

--- habit-tracker/backend/app/models/user.py
+++ habit-tracker/backend/app/models/user.py
@@ -1,11 +1,14 @@
-from beanie import Document
+from typing import Annotated 
+from beanie import Document, Indexed
 from datetime import datetime
+from pydantic import EmailStr, Field
 
 class User(Document):
-    email: str
+    email: Annotated[EmailStr, Indexed(unique=True)]
+
     hashed_password: str
     is_active: bool = True
-    created_at: datetime = datetime.utcnow()
+    created_at: datetime = Field(default_factory = datetime.utcnow)
 
     class Settings:
         name = "users"
