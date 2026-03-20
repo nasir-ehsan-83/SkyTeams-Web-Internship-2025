22e849e55d04400a134db856e48bd8a52415fad2
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-20 11:02:42 +0430

    feat: add hash() and verify() to hash password with passlib and 'bcrypt'.

---
+++ habit-tracker/backend/app/core/security.py
@@ -0,0 +1,14 @@
+from passlib.context import CryptContext
+
+password_context = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
+
+def hash(password: str) -> str:
+    password_bytes = password.encode("utf-8")[:72]
+    password_truncated = password_bytes.decode("utf-8", errors = "ignore")
+
+    return password_context.hash(password_truncated)
+
+def verify(plain_password: str, hashed_password: str) -> bool:
+    if not hashed_password:
+        return False
+    return password_context.verify(plain_password, hashed_password)
