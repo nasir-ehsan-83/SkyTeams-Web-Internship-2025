f07607ced02c78d49b2b64eaaf1fede498d65b2f
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-26 18:38:59 +0430

    feat: add username field in in User model

--- habit-tracker/backend/app/models/user.py
+++ habit-tracker/backend/app/models/user.py
@@ -5,7 +5,7 @@ from pydantic import EmailStr, Field
 
 class User(Document):
     email: Annotated[EmailStr, Indexed(unique=True)]
-
+    username: str
     hashed_password: str
     is_active: bool = True
     created_at: datetime = Field(default_factory = datetime.utcnow)
