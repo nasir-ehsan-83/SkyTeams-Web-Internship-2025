79f415960dee2cc0aaa376d87f93b1c266748a38
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-26 18:42:43 +0430

    feat: add UserBase class as the parent of UserCreate and UserResponse and add UserUpdate class to manage updating users schemas

--- habit-tracker/backend/app/schemas/user.py
+++ habit-tracker/backend/app/schemas/user.py
@@ -1,14 +1,22 @@
-from pydantic import BaseModel, EmailStr, ConfigDict
+from typing import Optional
+from pydantic import BaseModel, EmailStr, ConfigDict, Field
 from datetime import datetime
 
-class UserCreate(BaseModel):
+class UserBase(BaseModel):
+    username: str = Field(min_length = 3, max_length = 30)
     email: EmailStr
-    password: str
+
+class UserCreate(BaseModel):
+    password: str = Field(min_length = 8)
 
 class UserResponse(BaseModel):
-    id: str
-    email: EmailStr
-    is_active: bool
+    id: Optional[str] = Field(alias = "_id")
     created_at: datetime
+    is_active: bool
 
-    model_config = ConfigDict(from_attributes = True)
+    model_config = ConfigDict(from_attributes = True, populate_by_name = True)
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = Field(min_length = 3, max_length = 30)
+    email: Optional[EmailStr]
+    password: Optional[str] = Field(min_length = 8)
