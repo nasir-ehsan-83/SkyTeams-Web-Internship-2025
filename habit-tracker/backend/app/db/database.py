da3f7deb79688c5850b2588cbec23ed4948918db
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-03-28 17:10:40 +0430

    feat: remove unneccesary import

--- habit-tracker/backend/app/db/database.py
+++ habit-tracker/backend/app/db/database.py
@@ -3,12 +3,11 @@ from beanie import init_beanie
 
 from app.core.confing import settings
 from app.models.user import User
-from app.models.habit import Habit
 
 async def init_db():
     client = AsyncIOMotorClient( f'mongodb://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
 
     await init_beanie(
         database = client[settings.database_name],
-        document_models = [User, Habit]
+        document_models = [User]
     )
