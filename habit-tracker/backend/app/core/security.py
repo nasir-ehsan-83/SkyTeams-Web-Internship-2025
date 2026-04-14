104691de0c587f65cf6de072e2944761ba58878a
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-14 21:10:15 +0430

    fix: fix the run_in_threadpool() arquments

--- habit-tracker/backend/app/core/security.py
+++ habit-tracker/backend/app/core/security.py
@@ -1,12 +1,15 @@
 from passlib.context import CryptContext
+from fastapi.concurrency import run_in_threadpool
 
 password_context = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
 
 async def hash(password: str) -> str:
-    password_bytes = await password.encode("utf-8")[:72]
-    password_truncated = await password_bytes.decode("utf-8", errors = "ignore")
+    password_bytes = password.encode("utf-8")[:72]
 
-    return await password_context.hash(password_truncated)
+    password_truncated = password_bytes.decode("utf-8", errors="ignore")
+
+    # Offload the heavy hashing to a threadpool
+    return await run_in_threadpool(password_context.hash, password_truncated)
 
 async def verify(plain_password: str, hashed_password: str) -> bool:
     if not hashed_password:
