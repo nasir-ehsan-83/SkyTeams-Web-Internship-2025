664d6c216a6ca5816a0ac44e2dae187e2cc56c2a
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-14 21:19:57 +0430

    refactor: add additional fields User Model and update last fields in models/user.py

--- habit-tracker/backend/app/models/user.py
+++ habit-tracker/backend/app/models/user.py
@@ -4,11 +4,13 @@ from datetime import datetime
 from pydantic import EmailStr, Field
 
 class User(Document):
-    email: Annotated[EmailStr, Indexed(unique=True)]
+    name: str
     username: str
-    hashed_password: str
+    email: Annotated[EmailStr, Indexed(unique=True)]
+    password: str
     is_active: bool = True
     created_at: datetime = Field(default_factory = datetime.utcnow)
+    updated_at: datetime = Field(default_factory = datetime.utcnow)
 
     class Settings:
         name = "users"
