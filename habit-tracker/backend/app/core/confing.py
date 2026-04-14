4e1511cc9af87492fa5af5f26ef4a2dce61e3562
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-14 21:07:57 +0430

    refactor: update Settings's fields

--- habit-tracker/backend/app/core/confing.py
+++ habit-tracker/backend/app/core/confing.py
@@ -1,15 +1,12 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_username: str
-    database_password: str
-    database_name: str
-
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    MONGO_URL: str
+    DATABASE_NAME: str
+    
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
