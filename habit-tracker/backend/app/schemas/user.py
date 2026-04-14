da2233ea999b3264ed00e368a3011dfe6ea99a5c
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-14 21:30:39 +0430

    refactor(user): update User schemas and schema configuration

--- habit-tracker/backend/app/schemas/user.py
+++ habit-tracker/backend/app/schemas/user.py
@@ -1,8 +1,10 @@
 from typing import Optional
-from pydantic import BaseModel, EmailStr, ConfigDict, Field
+from pydantic import BaseModel, EmailStr, ConfigDict, Field, field_validator
 from datetime import datetime
+from bson import ObjectId 
 
 class UserBase(BaseModel):
+    name: str = Field(min_length = 3, max_length = 50)
     username: str = Field(min_length = 3, max_length = 30)
     email: EmailStr
 
@@ -10,13 +12,22 @@ class UserCreate(UserBase):
     password: str = Field(min_length = 8)
 
 class UserOut(UserBase):
-    id: Optional[str] = Field(alias = "_id")
+    id: Optional[str] = Field(alias="_id")
     created_at: datetime
     is_active: bool
 
-    model_config = ConfigDict(from_attributes = True, populate_by_name = True)
+    model_config = ConfigDict(from_attributes=True, populate_by_name=True)
+
+    @field_validator("id", mode="before")
+    @classmethod
+    def convert_objectid(cls, v):
+        if isinstance(v, ObjectId):
+            return str(v)
+        return v
+
 
 class UserUpdate(BaseModel):
-    username: Optional[str] = Field(min_length = 3, max_length = 30)
-    email: Optional[EmailStr]
-    password: Optional[str] = Field(min_length = 8)
+    name: Optional[str] = None
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
