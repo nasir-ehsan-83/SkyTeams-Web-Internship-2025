100f2c4f9ab8e643e3bcb00cbb5ad8b9bc7b3ebf
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-17 21:50:23 +0430

    feat(habit): implement HabitBase, HabitCreate, HabitOut and HabitUpdate schemas in schemas/habit.py

--- habit-tracker/backend/app/schemas/habit.py
+++ habit-tracker/backend/app/schemas/habit.py
@@ -3,26 +3,26 @@ from pydantic import BaseModel, ConfigDict, Field
 from datetime import datetime
 
 class HabitBase(BaseModel):
-    habit_name: str = Field(min_length = 3, max_length = 50)
-    habit_status: str
-    owner_id: int
+    name: str = Field(min_length = 3, max_length = 50)
+    status: str
     
     start_date: datetime
     end_date: datetime
 
-class HabitCreate(BaseModel):
+class HabitCreate(HabitBase):
     pass
 
 class HabitOut(HabitBase):
     id: int
+    owner_id: int
     created_at: datetime
     updated_at: datetime
 
     model_config = ConfigDict(from_attributes = True, populate_by_name = True)
 
 class HabitUpdate(BaseModel):
-    habit_name: Optional[str] = Field(default = None, min_lenght = 3, max_length = 50)
-    habit_status: Optional[str] = Field(default = None)
+    name: Optional[str] = Field(default = None, min_lenght = 3, max_length = 50)
+    status: Optional[str] = Field(default = None)
     
     star_date: Optional[datetime] = Field(default = None)
     end_date: Optional[datetime] = Field(default = None)
