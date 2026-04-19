bc62928b546ac34675c48ccf3d0b38c5a0310ffa
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-19 13:11:11 +0430

    feat(habit): update create_habit() logic in services/habit_service.py to prervent habit duplicate namming

--- habit-tracker/backend/app/services/habit_service.py
+++ habit-tracker/backend/app/services/habit_service.py
@@ -1,14 +1,30 @@
 from fastapi import HTTPException, status
+from pymongo.errors import DuplicateKeyError
 
 from app.models.habit import Habit
 from app.schemas.habit import HabitCreate
 
 async def create_new_habit(habit_in: HabitCreate, owner_id: int) -> Habit:
-    habit_data = habit_in.model_dump(exclude_unset = True, exclude_none = True)
-    habit_data.update({"owner_id": owner_id})
-    
-    new_habit = Habit(
-        **habit_data
-    )
+    existance_habit = await Habit.find_one({"name": habit_in.name})
 
-    return await new_habit.insert()
+    if existance_habit:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST, 
+            detail = f"habit with name: {habit_in.name} already exists"
+        )
+    
+    try:
+        habit_data = habit_in.model_dump(exclude_unset = True, exclude_none = True)
+        habit_data.update({"owner_id": owner_id})
+        
+        new_habit = Habit(
+            **habit_data
+        )
+
+        return await new_habit.insert()
+    
+    except DuplicateKeyError:
+        raise HTTPException(
+            status_code = status.HTTP_409_CONFLICT,
+            detail = "Data Conflict: The provided habit already exists"
+        )
