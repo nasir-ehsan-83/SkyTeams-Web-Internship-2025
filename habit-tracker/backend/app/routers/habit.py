d0cf27997cd553bed6f3486ab53f2ab2d1b34b73
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-19 20:20:44 +0430

    feat(habit): implement post, get, patch and delete methods for Habit model in routers/habit.py services/habit_service.py

--- habit-tracker/backend/app/routers/habit.py
+++ habit-tracker/backend/app/routers/habit.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
 
 from app.models.habit import Habit
-from app.schemas.habit import HabitCreate, HabitOut
-from app.services.habit_service import create_new_habit#, get_habit_by_id
+from app.schemas.habit import HabitCreate, HabitOut, HabitUpdate
+from app.services.habit_service import (
+    create_new_habit, 
+    get_habit_by_name,
+    udpdate_habit_by_name,
+    delete_habit_by_name
+)
 
 router = APIRouter(
     prefix = '/habits',
@@ -14,7 +19,17 @@ async def create_habit(habit: HabitCreate, owner_id: int) -> Habit:
     print(habit)
     return await create_new_habit(habit, owner_id)
 
-""" router.get('/{id}', response_model = HabitOut)
-async def get_habit(id: int, owner_id: int) -> Habit:
+@router.get('/name/{name}', response_model = HabitOut)
+async def get_habit(name: str, owner_id: int) -> Habit:
 
-    return await get_habit_by_id(id, owner_id) """
+    return await get_habit_by_name(name, owner_id)
+
+@router.patch('/name/{name}', response_model = HabitOut)
+async def update_habit(name: str, update_habit: HabitUpdate, owner_id: int) -> Habit:
+    
+    return await udpdate_habit_by_name(name, update_habit, owner_id)
+
+@router.delete('/name/{name}')
+async def delete_habit(name: str, owner_id: int) :
+
+    return await delete_habit_by_name(name, owner_id)
--- habit-tracker/backend/app/services/habit_service.py
+++ habit-tracker/backend/app/services/habit_service.py
@@ -1,8 +1,8 @@
-from fastapi import HTTPException, status
+from fastapi import HTTPException, Response, status
 from pymongo.errors import DuplicateKeyError
 
 from app.models.habit import Habit
-from app.schemas.habit import HabitCreate
+from app.schemas.habit import HabitCreate, HabitUpdate
 
 async def create_new_habit(habit_in: HabitCreate, owner_id: int) -> Habit:
     existance_habit = await Habit.find_one({"name": habit_in.name})
@@ -28,3 +28,46 @@ async def create_new_habit(habit_in: HabitCreate, owner_id: int) -> Habit:
             status_code = status.HTTP_409_CONFLICT,
             detail = "Data Conflict: The provided habit already exists"
         )
+    
+
+async def get_habit_by_name(name: str, owner_id: int) -> Habit:
+    existance_habit = await Habit.find_one({"name": name})
+
+    if not existance_habit:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"habit with name: {name} does not exists"
+        )
+    
+    return existance_habit
+
+async def udpdate_habit_by_name(name: str, update_habit: HabitUpdate, owener_id: int) -> Habit:
+    existance_habit = await Habit.find_one({"name": name})
+
+    if not existance_habit:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"habit with name: {name} does not exist"
+        )
+    
+    update_data = update_habit.model_dump(exclude_unset = True, exclude_none = True)
+    update_data.update({"owner_id": owener_id})
+
+    await existance_habit.update({"$set": update_data})
+    habit = await Habit.get(existance_habit.id)
+    
+    return habit
+
+async def delete_habit_by_name(name: str, owner_id: int):
+
+    existance_habit = await Habit.find_one({"name": name, "owner_id": owner_id})
+    
+    if not existance_habit:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"user with id: {owner_id} does not have any habit with name: {name}"
+        )
+    
+    await existance_habit.delete()
+
+    return Response(status_code = status.HTTP_204_NO_CONTENT)
