2f8bfc55d6d21c16c33f8067663d9c1dcd123884
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-20 23:44:56 +0430

    feat(habit): add addtional fields for HabitBase, HabitOut and HabitUpdate and remove datetime type and instead use date and time as types to represent date and time

--- habit-tracker/backend/app/schemas/habit.py
+++ habit-tracker/backend/app/schemas/habit.py
@@ -1,28 +1,28 @@
 from typing import Optional
 from pydantic import BaseModel, ConfigDict, Field
-from datetime import datetime
+from datetime import date, time
 
 class HabitBase(BaseModel):
     name: str = Field(min_length = 3, max_length = 50)
     status: str
-    
-    start_date: datetime = Field(default = datetime.utcnow)
-    end_date: datetime = Field(default = datetime.utcnow)
+    remind_time: time
+    start_date: date
+    end_date: date
 
 class HabitCreate(HabitBase):
     pass
 
 class HabitOut(HabitBase):
     _id: int
-    #owner_id: int
-    created_at: datetime
-    updated_at: datetime
+    owner_id: int
+    created_at: date
+    updated_at: date
 
     model_config = ConfigDict(from_attributes = True, populate_by_name = True)
 
 class HabitUpdate(BaseModel):
     name: Optional[str] = Field(default = None, min_lenght = 3, max_length = 50)
     status: Optional[str] = Field(default = None)
-    
-    start_date: Optional[datetime] = Field(default = None)
-    end_date: Optional[datetime] = Field(default = None)
+    remind: Optional[time] = Field(default = None)
+    start_date: Optional[date] = Field(default = None)
+    end_date: Optional[date] = Field(default = None)
