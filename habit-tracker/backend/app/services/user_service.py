7fc57ba79470c07530075a8c4f84f19e27feed6b
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-21 19:50:45 +0430

    feat(user-service): update user_service.py update variables names. add updated_at feild to update this field in database by every update time

--- habit-tracker/backend/app/services/user_service.py
+++ habit-tracker/backend/app/services/user_service.py
@@ -1,37 +1,38 @@
 from fastapi import HTTPException, Response, status
 from pymongo.errors import DuplicateKeyError
+from datetime import datetime, timezone
 
 from app.core.security import hash, verify
 from app.schemas.user import UserCreate, UserUpdate
 from app.models.user import User
 
-async def create_user(user_in: UserCreate):
-    print(user_in)
-    exists_email = await User.find_one({"email": user_in.email})
+async def create_user(user: UserCreate):
+
+    exists_email = await User.find_one({"email": user.email})
 
     if exists_email:
         raise HTTPException(
             status_code = status.HTTP_409_CONFLICT, 
-            detail = f"User with email: {user_in.email} already exists"
+            detail = f"User with email: {user.email} already exists"
         )
     
-    exists_username = await User.find_one({"username": user_in.username})
+    exists_username = await User.find_one({"username": user.username})
 
     if exists_username:
         raise HTTPException(
             status_code = status.HTTP_409_CONFLICT, 
-            detail = f"User with username: {user_in.username} already exists"
+            detail = f"User with username: {user.username} already exists"
         )
     
     try :
         # hash password before storing
-        hashed_password = await hash(user_in.password)
+        hashed_password = await hash(user.password)
         
         # create new user 
         new_user = User(
-            name = user_in.name,
-            username = user_in.username,
-            email = user_in.email, 
+            name = user.name,
+            username = user.username,
+            email = user.email, 
             password = hashed_password
         )
 
@@ -44,42 +45,45 @@ async def create_user(user_in: UserCreate):
             detail = f"Data conflict: The provided credentials are already in use."
         )
 
-async def get_user_by_email(user_in_email: str) -> User:
-    user = await User.find_one({"email": user_in_email})
+async def get_user_by_email(email: str) -> User:
+    user = await User.find_one({"email": email})
 
     if not user:
         raise HTTPException(
             status_code = status.HTTP_404_NOT_FOUND, 
-            detail = f"User with email: {user_in_email} does not exist"
+            detail = f"User with email: {email} does not exist"
         )
     
     return user
 
-async def update_user_by_email(user_in_email: str, user_update: UserUpdate) -> User:
-    user = await User.find_one(User.email == user_in_email)
+async def update_user_by_email(user_update: UserUpdate) -> User:
+    user = await User.find_one(User.email == user_update.email)
 
     if not user_update:
         return user
     
-    update_data = user_update.model_dump(exclude_unset = True, exclude_none = True)
+    update_data = user_update.update_data.model_dump(exclude_unset = True, exclude_none = True)
     
     if "password" in update_data:
         update_data["password"] = await hash(update_data["password"])
+
+    # add update_at to the data
+    update_data.update({"updated_at": datetime.now(timezone.utc)})
 
     # also you can use await user.set(update_data)
     await user.update({ "$set" : update_data})
     user = await User.get(user.id)
     return user
 
-async def delete_user_by_id(user_in_email):
+async def delete_user_by_id(email):
     # find the user from database
-    user = await User.find_one(User.email == user_in_email)
+    user = await User.find_one(User.email == email)
 
     # if user does not exist
     if not user:
         raise HTTPException(
             status_code = status.HTTP_404_NOT_FOUND,
-            detail = f"User with email: {user_in_email} does not exists"
+            detail = f"User with email: {email} does not exists"
         )
     # delete the user from database
     await user.delete()
