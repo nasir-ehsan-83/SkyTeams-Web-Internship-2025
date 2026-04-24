02f0b0cdce4c1ace4a6f42f1fe4af8141044a3e0
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-24 07:54:02 +0430

    feat(jwt-token): implement jwt token creation logic in core/oauth2.py and schemas/token.py

---
+++ habit-tracker/backend/app/core/oauth2.py
@@ -0,0 +1,47 @@
+from fastapi.concurrency import run_in_threadpool
+from jose import jwt, JWTError
+from datetime import datetime, timezone, timedelta
+from typing import Dict
+
+from app.schemas.token import TokenData
+from app.core.confing import settings
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+
+async def create_access_token(data: Dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = await run_in_threadpool(
+        jwt.encode,
+        to_encode,
+        SECRET_KEY,
+        algorithm = ALGORITHM
+    )
+
+    return encoded_jwt
+
+async def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = await run_in_threadpool(
+            jwt.decode,
+            token,
+            SECRET_KEY,
+            algorithm = ALGORITHM
+        )
+
+        user_id: str = payload.get("user_id")
+
+        if user_id is None:
+            raise credentials_exception
+        
+        token_data = TokenData(id = str(user_id))
+
+    except JWTError:
+        raise credentials_exception
+    
+    return token_data
