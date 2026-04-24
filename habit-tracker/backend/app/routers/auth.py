81716802dd0639c5850cdbda1dae017bbe498a94
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-24 08:20:36 +0430

    feat(auth): implement login router and logic in routers/auth.py and services/auth_service.py

---
+++ habit-tracker/backend/app/routers/auth.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from typing import Annotated
+
+from app.schemas.token import Token
+from app.services.auth_service import login
+
+router = APIRouter(
+    tabs = ["Authentication"]
+)
+
+@router.post('/login', response_model = Token)
+async def user_login(user_credential: Annotated[OAuth2PasswordRequestForm, Depends()]):
+
+    return await login(user_credential)
---
+++ habit-tracker/backend/app/services/auth_service.py
@@ -0,0 +1,34 @@
+from fastapi import HTTPException, status
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm 
+
+from app.models.user import User
+from app.core.security import verify
+
+from app.core.oauth2 import create_access_token
+
+async def login(user_credential: OAuth2PasswordRequestForm) :
+    # get user from database
+    existance_user = await User.find_one({"usrename": user_credential.username})
+
+    # if specific user does not exist
+    if not existance_user:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Invalid user credential"
+        )
+
+    # else verify password
+
+    if not await verify(user_credential.password, existance_user.password):
+        raise HTTPException (
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Invalid password credential"
+        )
+
+    # create JWT token
+    access_token = create_access_token(data = {"user_id": existance_user.id})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
