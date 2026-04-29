71dc0d281f50ecc57f22f70bc9c42652e2445aac
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-29 22:56:10 +0430

    refactor(habit-service): add habit-status check for all database logics in services/habit_service.py

--- habit-tracker/backend/app/services/habit_service.py
+++ habit-tracker/backend/app/services/habit_service.py
@@ -1,109 +1,135 @@
 from typing import List, Optional
 from fastapi import HTTPException, Response, status
 from pymongo.errors import DuplicateKeyError
+from datetime import datetime, timezone
 
 from app.models.habit import Habit
 from app.schemas.habit import HabitCreate, HabitUpdate
+from app.schemas.token import TokenData
 
-async def create_new_habit(habit_in: HabitCreate, current_user: int) -> Habit:
+async def create_new_habit(habit_in: HabitCreate, current_user: TokenData) -> Habit:
     # get habit from database by specific owner
-    existing_habit = await Habit.find_one({
-        "name": habit_in.name.strip(),
-        "owner_id": int(current_user.id)
-    })
+    existing_habit = await Habit.find_one(
+        Habit.name == habit_in.name,
+        Habit.owner_id == int(current_user.id),
+        Habit.status != "deleted"
+    )
     
     # if user already has habit
     if existing_habit:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"You already have a habit named '{habit_in.name}'"
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "Habit already exists"
         )
 
     try:
-        habit_data = habit_in.model_dump(exclude_unset=True, exclude_none=True)
-        
-        if "remind_time" in habit_data and hasattr(habit_data["remind_time"], "strftime"):
-            habit_data["remind_time"] = habit_data["remind_time"].strftime("%H:%M")
-            
-        habit_data.update({"owner_id": int(current_user.id), "name": habit_in.name.strip()})
-        
-        new_habit = Habit(**habit_data)
+        new_habit = Habit(
+            **habit_in.model_dump(),
+            owner_id = int(current_user.id)
+        )
+
         return await new_habit.insert()
 
     except DuplicateKeyError:
        
         raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Conflict: A habit with this name already exists for this user."
+            status_code = status.HTTP_409_CONFLICT,
+            detail = "Conflict: A habit with this name already exists for this user."
         )
 
-async def get_all_habits_owner(current_user: int) -> List[Habit]:
+
+async def get_all_habits_owner(current_user: TokenData) -> List[Habit]:
     # get all habits of users
-    habits = await Habit.find_all({"owner_id": int(current_user.id)})
-    
-    return habits
+    return  await Habit.find_all(Habit.owner_id == int(current_user.id), Habit.status != "deleted").to_list()
 
-async def get_all_habits_admin(owner_id: Optional[int] = None) -> List[Habit]:
+
+async def get_all_habits_admin(current_user: TokenData, owner_id: Optional[int] = None) -> List[Habit]:
     # get all habits of specific user
+    if current_user.role is not "admin":
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Invalid credential"
+        )
+    
     if owner_id is not None:
-        habits = await Habit.find_all({"owner_id": owner_id})
-
-        return habits
+        return await Habit.find_all(Habit.owner_id == owner_id).to_list()
 
     # else get all habits of all users 
-    habits = await Habit.find_all()
+    return await Habit.find_all().to_list()
 
-    return habits
 
 async def get_habit_by_name(name: str, current_user: int) -> Habit:
     # get habit by specific owner
-    existance_habit = await Habit.find_one({"name": name, "owner_id": int(current_user.id)})
+    existing_habit = await Habit.find_one(
+        Habit.name == name,
+        Habit.owner_id == int(current_user.id),
+        Habit.status != "deleted"   
+    )
 
     # if habit does not exist
-    if not existance_habit:
+    if not existing_habit:
         raise HTTPException(
             status_code = status.HTTP_404_NOT_FOUND,
-            detail = f"habit with name: {name} does not exists"
+            detail = "Habit not found"
         )
     
     # if habit exists then return 
-    return existance_habit
+    return existing_habit
 
-async def udpdate_habit_by_name(name: str, update_habit: HabitUpdate, current_user: int) -> Habit:
+async def udpdate_habit_by_name(name: str, update_habit: HabitUpdate, current_user: TokenData) -> Habit:
     # get specific habit form database
-    existance_habit = await Habit.find_one({"name": name, "owner_id": int(current_user.id)})
+    existing_habit = await Habit.find_one(
+        Habit.name == name,
+        Habit.owner_id == int(current_user.id),
+        Habit.status != "deleted"
+    )
 
     # if habit does not exist
-    if not existance_habit:
+    if not existing_habit:
         raise HTTPException(
             status_code = status.HTTP_404_NOT_FOUND,
-            detail = f"habit with name: {name} does not exist"
+            detail = "Habit not found"
         )
     
     # delete undefined or null values
-    update_data = update_habit.model_dump(exclude_unset = True, exclude_none = True)
-    update_data.update({"owner_id": int(current_user.id)})
+    update_data = update_habit.model_dump(
+        exclude_unset = True, 
+        exclude_none = True
+    )
 
-    # update habit and  store in database
-    await existance_habit.update({"$set": update_data})
-    habit = await Habit.get(existance_habit.id)
+    update_data.update({
+        "updated_at": datetime.now(timezone.utc)
+    })
     
-    # return habit
-    return habit
+    # update habit and  store in database
+    await existing_habit.update({
+        "$set": update_data
+    })
+    
+    return await Habit.get(existing_habit.id)
+    
 
-async def delete_habit_by_name(name: str, current_user: int):
+async def delete_habit_by_name(name: str, current_user: TokenData):
     # get specific habit from database
-    existance_habit = await Habit.find_one({"name": name, "owner_id": int(current_user.id)})
+    existing_habit = await Habit.find_one(
+        Habit.name == name,
+        Habit.owner_id == int(current_user.id),
+        Habit.status != "deleted"
+    )
     
     # if specific habit does not exist
-    if not existance_habit:
+    if not existing_habit:
         raise HTTPException(
             status_code = status.HTTP_404_NOT_FOUND,
-            detail = f"user with id: {current_user.id} does not have any habit with name: {name}"
+            detail = "Habit not found"
         )
     
     # if exists delete
-    await existance_habit.delete()
+    await existing_habit.update({
+        "$set": {
+            "status": "deleted"
+        }
+    })
 
     # return nothing
     return Response(status_code = status.HTTP_204_NO_CONTENT)
