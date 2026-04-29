ad421458d745c57f94b89d84d103fe681049234d
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-04-29 22:18:07 +0430

    refactor(auth): update getting user logic from database

--- habit-tracker/backend/app/services/auth_service.py
+++ habit-tracker/backend/app/services/auth_service.py
@@ -3,32 +3,31 @@ from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 from app.models.user import User
 from app.core.security import verify
-
 from app.core.oauth2 import create_access_token
 
 async def login(user_credential: OAuth2PasswordRequestForm) :
     # get user from database
-    existance_user = await User.find_one({"username": user_credential.username})
+    user = await User.find_one(User.username == user_credential.username)
 
     # if specific user does not exist
-    if not existance_user:
+    if not user:
         raise HTTPException(
             status_code = status.HTTP_404_NOT_FOUND,
-            detail = "Invalid user credential"
+            detail = "Invalid credential"
         )
 
     # else verify password
 
-    if not await verify(user_credential.password, existance_user.password):
+    if not await verify(user_credential.password, user.password):
         raise HTTPException (
             status_code = status.HTTP_404_NOT_FOUND,
-            detail = "Invalid password credential"
+            detail = "Invalid credential"
         )
 
     # create JWT token
     access_token = await create_access_token(data = {
-        "user_id": existance_user.id,
-        "role": existance_user.role
+        "user_id": str(user.id),
+        "role": user.role
     })
 
     return {
